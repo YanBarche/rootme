41a9342aecb4a6471921eabd0e44fcd3d1b3ecb9
root <root@localhost.localdomain>
2019-11-26 22:39:06 +0100

    test

---
+++ git/csrf.php
@@ -0,0 +1,14 @@
+
+<form method="post" action="?action=login">
+<div>
+<input name="username" placeholder="Username" type="text">
+</div>
+<br>
+<div>
+<input name="password" placeholder="Password" type="password">
+</div>
+<br>
+<button type="submit">Sign in</button>
+</form>
+
+
